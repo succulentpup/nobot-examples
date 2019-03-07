db00ea223eaf73392f2fdce583429a18cdf185b5
CivilBot <civilbots@gmail.com>
2019-03-07 22:40:57 +0530

    used stdin & hanlded readable event

--- examples/002/myProcess.js
+++ examples/002/myProcess.js
@@ -1,4 +1,12 @@
-process.stdout.write('Hello I am writing to standard output\n');
-process.stdout.write(`Current working directory is ${process.cwd()}\n`);
-process.stdout.write(`This script is up & running for ${process.uptime()} seconds\n`);
+process.stdout.write('Type something and hit enter\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null && chunk !== undefined) {
+    process.stdout.write(`Following is the string input by user:
+    ${chunk}
+    `);
+    process.exit(0);
+  }
+});
 process.on('exit', code => console.log(`process with PID:${process.pid} is finished now with code ${code}`));
