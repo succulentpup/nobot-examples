640554cccc34d21f5de2e6a5990f587bc3ab07c2
CivilBot <civilbots@gmail.com>
2019-03-07 22:32:36 +0530

    learnt using standard output

--- examples/002/myProcess.js
+++ examples/002/myProcess.js
@@ -1,1 +1,4 @@
-process.on('exit', code => console.log(`process with ${process.pid} is finished now with code ${code}`));
+process.stdout.write('Hello I am writing to standard output\n');
+process.stdout.write(`Current working directory is ${process.cwd()}\n`);
+process.stdout.write(`This script is up & running for ${process.uptime()} seconds\n`);
+process.on('exit', code => console.log(`process with PID:${process.pid} is finished now with code ${code}`));
